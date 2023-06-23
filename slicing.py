896332e2b2ffbb77aaa8ea3cfc8ace924b150e55
kim-thuita <kevinkoech265@gmail.com>
2023-06-23 18:54:50 +0300

    "Implementation/Working with Slices "

---
+++ slicing.py
@@ -0,0 +1,22 @@
+# Working with slices
+
+list1 = [12, 3, 4, 6, 7, 71, 2, 3, 4, 6, 7, 8, 9, 0]
+
+# The Sice method takes three params start,stop and a step
+
+# Reversing and printing out steps of 2
+print(list1[::-2])
+
+# Working with Strings
+
+request = 'https://Woolan.com'
+
+# Printing out the https only
+print(request[:5])
+
+# Printing out the domain
+print(request[8:])
+
+# Reversing the String name
+
+print(request[::-1])
